e9267479a468db2f22bfca612faf38997c1bf0b7
MidKnightXI <romain.bouguet@epitech.eu>
2023-02-16 00:29:39 +0900

    feat: adding typing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::task;
-use reqwest::Client;
+use reqwest::{Client, Response};
 
 #[tokio::main]
 async fn main() {
@@ -16,31 +16,33 @@ async fn main() {
         .nth(2)
         .unwrap()
         .parse::<usize>()
-        .expect("Failed to parse number of threads");
+        .expect("Failed to parse number of threads.");
 
     let num_requests: usize = std::env::args()
         .nth(3)
         .unwrap()
         .parse::<usize>()
-        .expect("Failed to parse number of requests");
+        .expect("Failed to parse number of requests.");
 
     // Create shared variables to store the results
-    let success = Arc::new(Mutex::new(0));
-    let failure = Arc::new(Mutex::new(0));
+    let success: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
+    let failure: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
 
     // Spawn the threads
     for _ in 0..num_threads
     {
-        let success = success.clone();
-        let failure = failure.clone();
+        let success: Arc<Mutex<i32>> = success.clone();
+        let failure: Arc<Mutex<i32>> = failure.clone();
 
         task::spawn(async move {
-            let url = std::env::args().nth(1).unwrap();
-            let client = Client::new();
+            let url: String = std::env::args()
+                .nth(1)
+                .expect("Failed to parse the URL.");
+            let client: Client = Client::new();
 
             for _ in 0..num_requests
             {
-                let response = client.get(url.as_str())
+                let response: Response = client.get(url.as_str())
                     .send()
                     .await
                     .unwrap();
