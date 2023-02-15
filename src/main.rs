3199e77cfab8810867051810bff363895b959391
MidKnightXI <romain.bouguet@epitech.eu>
2023-02-16 00:10:32 +0900

    feat: removing redundant code + adding arg verif

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::sync::Arc;
-use tokio::sync::mpsc;
 use tokio::sync::Mutex;
 use tokio::task;
 use reqwest::Client;
 
 #[tokio::main]
 async fn main() {
+
+    if std::env::args().nth(3).is_none()
+    {
+        eprintln!("Not enough argument specified.");
+        return;
+    }
+
     let num_threads: usize = std::env::args()
         .nth(2)
         .unwrap()
@@ -18,16 +24,13 @@ async fn main() {
         .parse::<usize>()
         .expect("Failed to parse number of requests");
 
-    // Create a channel to receive responses from the threads
-    let (tx, mut rx) = mpsc::channel(num_threads * num_requests);
-
     // Create shared variables to store the results
     let success = Arc::new(Mutex::new(0));
     let failure = Arc::new(Mutex::new(0));
 
     // Spawn the threads
-    for _ in 0..num_threads {
-        let tx = tx.clone();
+    for _ in 0..num_threads
+    {
         let success = success.clone();
         let failure = failure.clone();
 
@@ -35,38 +38,24 @@ async fn main() {
             let url = std::env::args().nth(1).unwrap();
             let client = Client::new();
 
-            for _ in 0..num_requests {
+            for _ in 0..num_requests
+            {
                 let response = client.get(url.as_str())
                     .send()
                     .await
                     .unwrap();
 
-                // Send the response status back through the channel
-                tx.send(response.status()
-                    .as_u16())
-                    .await
-                    .unwrap();
-
                 // Update the results
-                if response.status().is_success() {
+                if response.status().is_success()
+                {
                     *success.lock().await += 1;
-                } else {
+                }
+                else
+                {
                     *failure.lock().await += 1;
                 }
             }
         });
-    }
-
-    let mut counter = 0;
-
-    while counter < num_threads * num_requests {
-        let status = rx.recv().await.unwrap();
-        if status == 200 {
-            *success.lock().await += 1;
-        } else {
-            *failure.lock().await += 1;
-        }
-        counter += 1;
     }
 
     // Print the results
